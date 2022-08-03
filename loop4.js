6e8972d7a151db3fe79076f23fe1d5d8d53d61d7
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2022-08-03 09:08:24 +0500

    file

---
+++ loop4.js
@@ -0,0 +1,6 @@
+function half(){
+    let my=document.getElementById("type").value;
+    for (let one = 1; one < 51; one++) {
+        document.write("<h1>"+  my +" x " + one + " = " + my*one +   "</h1>");
+        }
+    }
